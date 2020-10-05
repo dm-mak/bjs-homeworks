151be372da78d9ae15e542b15f8b7c68429401e3
lan-mak <d.makoseev@elec-co.ru>
2020-10-05 23:45:26 +0300

    Решение 1.2-extended-syntax v0.01a

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,55 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+  // код для задачи №1 писать здесь
+  // return x;
+  "use strict";
+  let d = Math.pow(b, 2) - 4 * a * c;
+  let x = [];
+
+  if (d === 0) {
+    x.push(-b / (2 * a));
+  } else if (d > 0) {
+    x.push((-b + Math.sqrt(d)) / (2 * a));
+    x.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+  return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+  // код для задачи №2 писать здесь
+  // return averageMark;
+  let averageMark;
+  let amountMark = 0;
+
+  if (marks.length === 0 || marks === undefined) {
+    averageMark = 0;
+  } else if (marks.length > 5) {
+    let rengeDelete = -marks.length + 5;
+    marks.splice(rengeDelete, -rengeDelete);
+    calculationMark();
+  } else if (marks.length <= 5) {
+    calculationMark();
+  }
+
+  function calculationMark() {
+    for (let i = 0; i < marks.length; i++) {
+      amountMark += marks[i];
+    }
+    averageMark = amountMark / marks.length;
+  }
+  return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+  // код для задачи №3 писать здесь
+  // return result;
+  let birthdayYear = dateOfBirthday.getFullYear();
+  let currentYear = new Date().getFullYear();
+  let age = currentYear - birthdayYear;
+  let result;
+  if (age >= 18) {
+    result = `Не желаете ли олд-фэшн, ${name}?`;
+  } else {
+    result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+  }
+  return result;
 }

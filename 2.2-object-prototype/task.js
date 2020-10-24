bb6c80efc1e094063c411437fc5fdfde414edcc1
lan-mak <d.makoseev@elec-co.ru>
2020-10-24 22:03:22 +0300

    Решение 2.2-functions v0.01

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,44 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let tempVar_1 = this.toLowerCase().split(' ').join('');
+    let tempVar_2 = tempVar_1.split('').reverse().join('');
+    return (tempVar_1 === tempVar_2);
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+    let roundedAverage = () => {
+        let average = 0
+        if (marks.length === 0) {
+            return average;
+        } else {
+            for (let i = 0; i < marks.length; i++) {
+                average += marks[i];
+            }
+            average = average / marks.length;
+            return Math.round(average);
+        }
+    }
+
+    return roundedAverage();
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+    let now = +new Date();
+    let dateUser = new Date(birthday);
+    birthday = +dateUser;
+    let diff = now - birthday;
+
+    let dateTemp = null;
+
+    if (((dateUser.getFullYear() % 4 === 0) && (dateUser.getFullYear() % 100 !== 0)) || (dateUser.getFullYear() % 400 === 0)) {
+        dateTemp = 1000 * 60 * 60 * 24 * 366;
+    } else {
+        dateTemp = 1000 * 60 * 60 * 24 * 365;
+    }
+
+    let age = diff / dateTemp;
+    age = Math.floor(age);
+
+    return verdict = age > 18 ? true : false;
 }

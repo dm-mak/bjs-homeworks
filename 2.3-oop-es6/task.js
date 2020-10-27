a3e93787d77363c9bbc17b2dfe875e026d997df9
lan-mak <d.makoseev@elec-co.ru>
2020-10-27 12:08:27 +0300

    Решение 2.3-oop-es6

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -94,32 +94,15 @@ class Library {
     }
 
     giveBookByName(bookName) {
-        let _giveBookByName = null;
-        for (let i = 0; i < this.books.length; i++) {
-            if (this.books[i].name === bookName) {
-                this.books.splice(i,1);
-                _giveBookByName = bookName;
-            } else {
-                _giveBookByName;
-            }
+        for (let i = 0; i < this.books.length; i += 1) {
+          if (this.books[i].name === bookName) {
+            return this.books.splice(i,1)[0];
+          }
         }
-        return _giveBookByName;
-    }
+        return null;
+      }
 }
 
-const library = new Library("Библиотека имени Ленина");
-
-// library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
-// library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
-// library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
-// library.addBook(new Magazine("Мурзилка", 1924, 60));
-
-// console.log(library.findBookBy("name", "Властелин колец")); //null
-// console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
-
-// console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
-// library.giveBookByName("Машина времени");
-// console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
 
 // Задача №3
 
